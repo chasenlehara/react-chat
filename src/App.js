6ff519d77f4fa9cdfd032f083ecfee36411386ba
Chasen Le Hara <chasen@chasenlehara.com>
2017-01-04 21:43:13 -0800

    Add Bootstrap & change the home page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="container">
+      <div className="row">
+        <div className="col-sm-8 col-sm-offset-2">
+          <h1 className="page-header text-center">
+            <img src={logo} alt="DoneJS logo" style={{width: '100%'}} />
+            <br />
+            Chat
+          </h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
+    </div>
     );
   }
 }
